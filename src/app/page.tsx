b4a810c38ad9b611be908634f0a8415d3edc4198
Ethan Markham <markham.ethan@gmail.com>
2024-05-29 07:23:54 -0400

    logo and manifest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,31 +6,30 @@ import Logo from "@/components/svg/Logo";
 
 export default function Home() {
   return (
-    <PageWrapper className="max-w-4xl mx-auto">
-      <hgroup className="text-center mb-8 space-y-2">
-        <h1 className="text-4xl font-bold">Welcome to Champions League RI</h1>
-        <h2 className="text-lg text-gray-700/70">Connecting Competitors, Creating Community.</h2>
+    <PageWrapper className="max-w-4xl mx-auto [&>*]:lg:mb-8 [&>*]:mb-4">
+      <hgroup className="text-center space-y-2">
+        <h1 className="text-xl lg:text-4xl font-bold">Welcome to Champions League RI</h1>
+        <h2 className="text-sm lg:text-lg text-gray-700/70">Connecting Competitors, Creating Community.</h2>
       </hgroup>
-      <p className="text-lg mb-8 text-center max-w-[400px]">
+      <p className="text-lg text-center max-w-[400px]">
         Join our community and participate in the league. Whether you&apos;re a seasoned pro or a
         beginner, we have something for everyone.
       </p>
-      <figure className="flex flex-col items-center mb-8">
-        <Logo className="w-64 h-auto" />
+      <figure className="flex flex-col items-center">
+        <Image src="/icons/logo.webp" alt="CLRI"  width={250} height={250} />
         <figcaption className="font-bold tracking-widest text-lg mt-4">Champions League RI</figcaption>
       </figure>
-      <section className="mb-8">
+      <section className="">
         <h3 className="text-xl font-semibold mb-2">Upcoming Events</h3>
         <p>
-          Stay tuned for our upcoming events. Check out our calendar and make sure to register early to secure your
-          spot.
+          Stay tuned for our upcoming events. Check out our calendar to see whats coming up!
         </p>
       </section>
-      <section className="mb-8">
+      <section className="">
         <h3 className="text-xl font-semibold mb-2">League Standings</h3>
-        <p>Follow the standings of your favorite players and teams. Get the latest updates on scores and rankings.</p>
+        <p>Get the latest updates on scores and rankings.</p>
       </section>
-      <div className="text-center">
+      <div className="text-center my-12">
         <Link href="/rules">
           <Button className="bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-full text-white">Learn More</Button>
         </Link>
--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -6,7 +6,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import MobileMenu from "@/components/nav/MobileMenu";
 import DesktopLinks from "@/components/nav/DesktopLinks";
 import LogoAlt from "@/components/svg/Logo";
-
+import Image from "next/image";
 export interface NavigationItem {
   name: string;
   href: string;
@@ -46,7 +46,8 @@ export const NavBar: React.FC = () => {
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
                 </MenuButton>
-                <LogoAlt className="w-16 grow-0" fill="white" />
+                <Image src="/icons/logo.webp" alt="CLRI" width={50} height={50}/>
+                {/* <LogoAlt className="w-16 grow-0" fill="white" /> */}
               </div>
               <DesktopLinks links={links} />
             </div>
