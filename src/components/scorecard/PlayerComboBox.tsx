ee21f345929443a6793623125bb40ee2ce20dcc0
Ethan Markham <markham.ethan@gmail.com>
2024-05-31 08:29:51 -0400

    ui

--- src/components/scorecard/PlayerComboBox.tsx
+++ src/components/scorecard/PlayerComboBox.tsx
@@ -12,7 +12,6 @@ import { ChevronDownIcon, PlusIcon, CheckCircleIcon } from "@heroicons/react/24/
 import { useState, Fragment } from "react";
 import { getUsers, UserSearchModel } from "@/lib/users";
 import AddPlayerModal from "../modals/AddPlayerModal";
-import { twMerge } from "tailwind-merge";
 import { useDebouncedEffect } from "@/hooks/useDebouncedEffect"; // Adjust the import path according to your project structure
 
 interface PlayerComboboxProps {
@@ -53,64 +52,62 @@ const PlayerCombobox: React.FC<PlayerComboboxProps> = ({ selected, setSelected }
 
   return (
     <>
-      <div className="flex items-center space-x-4">
-        <Combobox value={selected} onChange={handleSelect}>
-          <div className="relative w-full">
-            <div className="relative w-full cursor-default overflow-hidden rounded-lg border border-gray-300 bg-white text-left shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm">
-              <ComboboxInput
-                className="w-full border-none py-2 pr-10 pl-3 text-sm leading-5 text-gray-900 focus:ring-0 outline-none"
-                displayValue={(person: UserSearchModel) => person?.name || ""}
-                onChange={(event) => setQuery(event.target.value)}
-              />
-              <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2">
-                <ChevronDownIcon className="h-5 w-5 text-gray-400" />
-              </ComboboxButton>
-            </div>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <ComboboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {players.map((player) => (
-                  <ComboboxOption
-                    key={player.id}
-                    value={player}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? "bg-indigo-600 text-white" : "text-gray-900"
-                      }`
-                    }
-                  >
-                    {({ selected }) => (
-                      <>
-                        <span className={`block truncate ${selected ? "font-semibold" : "font-normal"}`}>
-                          {player.name}
-                        </span>
-                        {selected && (
-                          <span className={"absolute inset-y-0 left-0 flex items-center pl-3"}>
-                            <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
-                          </span>
-                        )}
-                      </>
-                    )}
-                  </ComboboxOption>
-                ))}
-              </ComboboxOptions>
-            </Transition>
+      <div className="flex items-center space-x-2">
+        <Combobox value={selected} onChange={handleSelect} as="div" className="relative w-full">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg border border-gray-300 bg-white text-left shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm">
+            <ComboboxInput
+              className="w-full border-none py-2 pr-10 pl-3 text-sm leading-5 text-gray-900 focus:ring-0 outline-none"
+              displayValue={(person: UserSearchModel) => person?.name || ""}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronDownIcon className="h-5 w-5 text-gray-400" />
+            </ComboboxButton>
           </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <ComboboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {players.map((player) => (
+                <ComboboxOption
+                  key={player.id}
+                  value={player}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-indigo-600 text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <>
+                      <span className={`block truncate ${selected ? "font-semibold" : "font-normal"}`}>
+                        {player.name}
+                      </span>
+                      {selected && (
+                        <span className={"absolute inset-y-0 left-0 flex items-center pl-3"}>
+                          <CheckCircleIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      )}
+                    </>
+                  )}
+                </ComboboxOption>
+              ))}
+            </ComboboxOptions>
+          </Transition>
         </Combobox>
         <button
           type="button"
           onClick={() => setIsModalOpen(true)}
           className="inline-flex items-center p-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          aria-label="Add Player"
         >
           <PlusIcon className="h-5 w-5" />
-          <span className="sr-only">Add Player</span>
         </button>
       </div>
       <AddPlayerModal isOpen={isModalOpen} closeModal={() => setIsModalOpen(false)} addPlayer={setSelected} />
--- src/components/scorecard/ScoreSheetBuilder.tsx
+++ src/components/scorecard/ScoreSheetBuilder.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { getTotalPar } from "@/lib/holes";
 import { getScoreColorClass } from "@/lib/score";
 import { getScoreSheetDetails, getTotalStrokes } from "@/lib/scorecard";
@@ -18,31 +18,33 @@ const ScoreSheetBuilder: React.FC<ScoreTableProps> = ({ scoreSheets, holes }) =>
   const totalScore = getTotalPar(holes);
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-6">
       {scoreSheets?.map(({ playerName, id, scores }, index) => (
         <div
           key={id}
-          className={`p-4 rounded-lg ${index % 2 === 0 ? "bg-gray-700 text-white" : "bg-gray-600 text-white"}`}
+          className={`p-6 rounded-lg shadow-md ${
+            index % 2 === 0 ? "bg-gray-700 text-white" : "bg-gray-600 text-white"
+          }`}
         >
-          <div className="flex items-center justify-between mb-2 gap-4">
-            <div className="flex-1 max-w-[280px] items-center space-y-2">
-              <div className="text-xs text-gray-400">UDisc: {playerName}</div>
-              <PlayerCombobox  setSelected={(people) => console.log(people)} />
+          <div className="flex items-center justify-between mb-4 gap-6">
+            <div className="flex-1 max-w-[280px]">
+              <div className="text-xs text-gray-400 mb-2">UDisc: {playerName}</div>
+              <PlayerCombobox setSelected={(people) => console.log(people)} />
             </div>
             <div className="text-center">
               <div className="text-sm font-light">Score:</div>
-              <div className="font-bold">{getTotalStrokes(scores) - totalScore}</div>
+              <div className="text-lg font-bold">{getTotalStrokes(scores) - totalScore}</div>
             </div>
           </div>
           <div className="flex flex-wrap justify-between gap-2">
             {scores.map((score, holeIndex) => (
-              <div key={holeIndex} className={`w-12 p-2 rounded ${getScoreColorClass(score, holes[holeIndex])}`}>
-                <div className="text-xs text-center text-gray-400">{holeIndex + 1}</div>
-                <div className="text-center">{score.score}</div>
+              <div key={holeIndex} className={`w-14 p-2 rounded ${getScoreColorClass(score, holes[holeIndex])}`}>
+                <div className="text-xs text-center text-gray-400">{score.holeNumber}</div>
+                <div className="text-center font-bold">{score.score}</div>
               </div>
             ))}
             <div className="flex-1">
-              <div className="w-24 p-2 rounded bg-gray-800 justify-end ml-auto">
+              <div className="w-24 p-2 rounded bg-gray-800 ml-auto text-white">
                 <div className="text-center font-bold">{getTotalStrokes(scores)}</div>
                 <div className="text-xs text-center text-gray-400">Total</div>
               </div>
